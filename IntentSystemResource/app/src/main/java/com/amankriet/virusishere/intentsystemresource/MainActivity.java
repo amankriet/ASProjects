ab8fd9905b3a3c5bea450793766bcaaeb9744512
amankriet <amankriet@gmail.com>
2018-07-16 13:45:37 +0530

    System Resource using Intent working phase 1

--- IntentSystemResource/app/src/main/java/com/amankriet/virusishere/intentsystemresource/MainActivity.java
+++ IntentSystemResource/app/src/main/java/com/amankriet/virusishere/intentsystemresource/MainActivity.java
@@ -2,15 +2,19 @@ package com.amankriet.virusishere.intentsystemresource;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaRecorder;
 import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends Activity {
 
     Button b1, b2;
+    ImageButton bi1, bi2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +22,8 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         b1=findViewById(R.id.bdial);
         b2=findViewById(R.id.bshowbrowser);
+        bi1=findViewById(R.id.bimg);
+        bi2=findViewById(R.id.bvid);
 
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,6 +40,26 @@ public class MainActivity extends Activity {
             public void onClick(View view) {
 
                 Intent i=new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.co.in"));
+                startActivity(i);
+
+            }
+        });
+
+        bi1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i=new Intent(MediaStore.ACTION_IMAGE_CAPTURE_SECURE);
+                startActivity(i);
+
+            }
+        });
+
+        bi2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i=new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
                 startActivity(i);
 
             }
