ea99f9508e2121a02193757ae086b3f654b4ae88
amankriet <amankriet@gmail.com>
2018-08-01 18:50:05 +0530

    Database app complete

--- UserDatabaase/app/src/main/java/com/amankriet/virusishere/userdatabaase/DisplayDetail.java
+++ UserDatabaase/app/src/main/java/com/amankriet/virusishere/userdatabaase/DisplayDetail.java
@@ -1,4 +1,184 @@
 package com.amankriet.virusishere.userdatabaase;
 
-public class DisplayDetail {
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class DisplayDetail extends AppCompatActivity {
+
+    int from_where_I_Am_Coming = 0;
+    TextView name;
+    TextView phone;
+    TextView email;
+    TextView city;
+    TextView country;
+    int id_To_Update = 0;
+    private DBHelper mydb;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.display_content);
+        name = findViewById(R.id.textname);
+        phone = findViewById(R.id.phonenum);
+        email = findViewById(R.id.textemail);
+        city = findViewById(R.id.edittextcity);
+        country = findViewById(R.id.edittextcountry);
+
+        mydb = new DBHelper(this);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            int Value = extras.getInt("id");
+            if (Value > 0) {
+                Cursor rs = mydb.getData(Value);
+                id_To_Update = Value;
+                rs.moveToFirst();
+                String nam = rs.getString(rs.getColumnIndex(DBHelper.CONTACTS_COLUMN_NAME));
+                String phon = rs.getString(rs.getColumnIndex(DBHelper.PHONE));
+                String emai = rs.getString(rs.getColumnIndex(DBHelper.EMAIL));
+                String cit = rs.getString(rs.getColumnIndex(DBHelper.CITY));
+                String countr = rs.getString(rs.getColumnIndex(DBHelper.COUNTRY));
+
+                if (!rs.isClosed()) {
+                    rs.close();
+                }
+                Button b = findViewById(R.id.button1);
+                b.setVisibility(View.VISIBLE);
+
+                name.setText(nam);
+                name.setFocusable(false);
+                name.setClickable(false);
+
+                phone.setText(phon);
+                phone.setFocusable(false);
+                phone.setClickable(false);
+
+                email.setText(emai);
+                email.setFocusable(false);
+                email.setClickable(false);
+
+                city.setText(cit);
+                city.setFocusable(false);
+                city.setClickable(false);
+
+                country.setText(countr);
+                country.setFocusable(false);
+                country.setFocusable(false);
+            }
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            int Value = extras.getInt("id");
+            if (Value > 0) {
+                getMenuInflater().inflate(R.menu.display_contact, menu);
+            } else {
+                getMenuInflater().inflate(R.menu.menu, menu);
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            case R.id.Edit_Contact:
+                Button b = findViewById(R.id.button1);
+                b.setVisibility(View.VISIBLE);
+                name.setEnabled(true);
+                name.setFocusableInTouchMode(true);
+                name.setClickable(true);
+
+                phone.setEnabled(true);
+                phone.setFocusableInTouchMode(true);
+                phone.setClickable(true);
+
+                email.setEnabled(true);
+                email.setFocusableInTouchMode(true);
+                email.setClickable(true);
+
+                city.setEnabled(true);
+                city.setFocusableInTouchMode(true);
+                city.setClickable(true);
+
+                country.setEnabled(true);
+                country.setFocusableInTouchMode(true);
+                country.setClickable(true);
+
+                return true;
+            case R.id.Delete_Contact:
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage("Delete Contact?").setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                        mydb.deleteContact(id_To_Update);
+                        Toast.makeText(getApplicationContext(), "Deleted Successfully", Toast.LENGTH_SHORT).show();
+
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(intent);
+
+                    }
+                }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                        //User Cancelled
+
+                    }
+                });
+                AlertDialog d = builder.create();
+                d.setTitle("Are you sure?");
+                d.show();
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void run(View view) {
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            int Value = extras.getInt("id");
+            if (Value > 0) {
+                if (mydb.updateContact(id_To_Update, name.getText().toString(), phone.getText().toString(),
+                        email.getText().toString(), city.getText().toString(), country.getText().toString())) {
+                    Toast.makeText(getApplicationContext(), "Updated", Toast.LENGTH_SHORT).show();
+
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Not Updated", Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                if (mydb.insertContact(name.getText().toString(), phone.getText().toString(),
+                        email.getText().toString(), city.getText().toString(), country.getText().toString())) {
+                    Toast.makeText(getApplicationContext(), "Done", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Not Done", Toast.LENGTH_SHORT).show();
+                }
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        }
+    }
+
 }
